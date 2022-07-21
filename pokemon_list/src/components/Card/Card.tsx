307c2c4cfe173a297d98ab46e554b09c463505bc
藤井真 <fuzimako0202@gmail.com>
2022-07-21 22:25:53 +0900

    Card .cssとNavber.cssを削除し、StyledComponents化しました

--- pokemon_list/src/components/Card/Card.tsx
+++ pokemon_list/src/components/Card/Card.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import "./Card.css";
+import styled from "styled-components";
 
  const Card = ({pokemon}) =>{
  return (
- <div className="Card">
+ <SCard >
   <div className="cardImg">
    <img src={pokemon.sprites.front_default} alt="" />
   </div>
-  <h3 className="cardName">{pokemon.name}</h3>
+  <ScardName>{pokemon.name}</ScardName>
   <div className="cardTypes">
    <div>タイプ</div>
    {pokemon.types.map((type)=>{
@@ -18,7 +19,7 @@ import "./Card.css";
     )
    })}
    </div>
-        <div className="cardInfo">
+        <ScardInfo>
          <div className="cardData">
           <p className="title">重さ：{pokemon.weigth}</p>
          </div>
@@ -28,9 +29,26 @@ import "./Card.css";
          <div className="cardData">
           <p className="title">アビリティ：{pokemon.abilities[0].ability.name}</p>
          </div>
-        </div>
- </div>
+        </ScardInfo>
+ </SCard >
  )
 }
+
+const SCard = styled.div`
+  width: 290px;
+  box-shadow: 24px 26px 22px 6px #777777;
+  border-radius: 10px;
+  background-color: #fff;
+`
+const ScardName = styled.h3`
+  padding:0;
+  font-size: 24px;
+  margin-bottom: 10px;
+  margin-top:0;
+`
+
+const ScardInfo = styled.div`
+  text-align: center;
+`
 
 export default Card;
